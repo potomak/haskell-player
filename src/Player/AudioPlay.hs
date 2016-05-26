817881f06108a738838cdafe3faf5f9cc7c7a6b0
Giovanni Cappellotto <potomak84@gmail.com>
2016-05-26 01:19:43 -0400

    Add pause, resume, and stop functions
    Turns out you can't start playing an audio file at a certain position, so
    let's implement `pause` and `resume` by sending signals to the currently
    running `afplay` process.
    
    See https://gist.github.com/agrberg/7a09dea9a4ac545decb7.

--- src/Player/AudioPlay.hs
+++ src/Player/AudioPlay.hs
@@ -1,10 +1,42 @@
 module Player.AudioPlay (
-  playSong
+  playSong,
+  pause,
+  resume,
+  stop
 ) where
 
-import GHC.IO.Handle (Handle)
-import System.Process (ProcessHandle, createProcess, proc)
+import Control.Monad (void)
+import Data.Maybe (fromJust)
+import System.Process (ProcessHandle, createProcess, proc, terminateProcess)
+import System.Process.Internals (ProcessHandle__(OpenHandle, ClosedHandle),
+  PHANDLE, withProcessHandle)
 
 
-playSong :: FilePath -> IO (Maybe Handle, Maybe Handle, Maybe Handle, ProcessHandle)
-playSong path = createProcess (proc "afplay" [path])
+playSong :: FilePath -> IO ProcessHandle
+playSong path = do
+  (_, _, _, processHandle) <- createProcess (proc "afplay" [path])
+  return processHandle
+
+
+pause :: ProcessHandle -> IO ()
+pause ph = do
+  mPid <- getPid ph
+  void $ createProcess (proc "kill" ["-17", show $ fromJust mPid])
+
+
+resume :: ProcessHandle -> IO ()
+resume ph = do
+  mPid <- getPid ph
+  void $ createProcess (proc "kill" ["-19", show $ fromJust mPid])
+
+
+stop :: ProcessHandle -> IO ()
+stop = terminateProcess
+
+
+-- See https://mail.haskell.org/pipermail/haskell-cafe/2012-October/104028.html
+getPid :: ProcessHandle -> IO (Maybe PHANDLE)
+getPid ph = withProcessHandle ph (return . go)
+  where
+    go (OpenHandle pid) = Just pid
+    go (ClosedHandle _) = Nothing
