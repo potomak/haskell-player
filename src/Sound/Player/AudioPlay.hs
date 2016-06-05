7a8b00df0eeb098f272ffb4e6a21ce6ef72794e4
Giovanni Cappellotto <potomak84@gmail.com>
2016-06-05 16:16:53 -0400

    Use signalProcess to send signals and add docs

--- src/Sound/Player/AudioPlay.hs
+++ src/Sound/Player/AudioPlay.hs
@@ -5,14 +5,17 @@ module Sound.Player.AudioPlay (
   stop
 ) where
 
-import Control.Monad (void)
-import Data.Maybe (fromJust)
 import System.Process (ProcessHandle, StdStream(CreatePipe),
   CreateProcess(std_err), createProcess, proc, terminateProcess)
+import System.Posix (ProcessID)
+import System.Posix.Signals (signalProcess)
 import System.Process.Internals (ProcessHandle__(OpenHandle, ClosedHandle),
-  PHANDLE, withProcessHandle)
+  withProcessHandle)
 
 
+-- | Creates an @afplay@ process to play file at @path@.
+--
+-- Note: it suppresses process' @stderr@.
 play :: FilePath -> IO ProcessHandle
 play path = do
   (_, _, _, processHandle) <-
@@ -23,24 +26,28 @@ play path = do
   return processHandle
 
 
+-- | Sends a @17@ signal to @ph@'s process. If @ph@ is the handle of a running
+-- @afplay@ process it will pause playback.
 pause :: ProcessHandle -> IO ()
-pause ph = do
-  mPid <- getPid ph
-  void $ createProcess (proc "kill" ["-17", show $ fromJust mPid])
+pause ph =
+  maybe (return ()) (signalProcess 17) =<< getPid ph
 
 
+-- | Sends a @19@ signal to @ph@'s process. If @ph@ is the handle of a running
+-- @afplay@ process it will resume playback.
 resume :: ProcessHandle -> IO ()
-resume ph = do
-  mPid <- getPid ph
-  void $ createProcess (proc "kill" ["-19", show $ fromJust mPid])
+resume ph =
+  maybe (return ()) (signalProcess 19) =<< getPid ph
 
 
+-- | Terminates the selected process. If @ph@ is the handle of a running
+-- @afplay@ process it will stop playback.
 stop :: ProcessHandle -> IO ()
 stop = terminateProcess
 
 
 -- See https://mail.haskell.org/pipermail/haskell-cafe/2012-October/104028.html
-getPid :: ProcessHandle -> IO (Maybe PHANDLE)
+getPid :: ProcessHandle -> IO (Maybe ProcessID)
 getPid ph = withProcessHandle ph (return . go)
   where
     go (OpenHandle pid) = Just pid
